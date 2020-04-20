08628d56fcc904ad8dc9de5de074f7e739dac2ef
Alexander Binder <s1710306004@students.fh-hagenberg.at>
2020-04-20 21:23:46 +0200

    generated hex file for system test

---
+++ test/checkSystem.c
@@ -0,0 +1,39 @@
+#include <stdbool.h>
+
+#define ADDR_LEDS   ((char *) 0x40000000)
+#define ADDR_SWITCH ((char *) 0x40000010)
+
+bool ledState = false;
+
+void toggleLeds(){
+
+    if(ledState){
+        *(ADDR_LEDS + 1) = 0x2;
+    } else {
+        *(ADDR_LEDS + 1) = 0x1;
+    }
+
+    ledState = !ledState;
+}
+
+int main(){
+    char *pLeds      = ADDR_LEDS;
+    char *pSwitches  = ADDR_SWITCH;
+    long i = 0;
+
+    while(1){
+        
+        *pSwitches = *pLeds;
+    
+        // 1 Hz bei 50 MHz
+        if (i == 25000000) {
+            toggleLeds();
+            i = 0;
+        } else {
+            i++;
+        }
+
+    }
+
+    return 0;
+}
